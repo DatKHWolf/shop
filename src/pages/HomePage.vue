6b94d557f6d9c42b3d579ab49c77b2fe52293059
DatKHWOLF <dkhwolf@gmail.com>
2024-10-30 14:22:40 +0100

    First commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <TheTwoColumnsLayout
+    :leftColumnClass="'col-md-8 bg-vue'"
+    :rightColumnClass="'col-md-4'"
+    :fullsize="true"
+  >
+  <template #leftCol>
+    <div class="mt-5">
+        <div class="display-1 my-5">Der Shop</div>
+        <div class="display-4 my-5">
+            Erhalten Sie Zugriff auf exklusive Artikel
+        </div>
+        <div class="my-5 offset-4 col-2">
+            <img src="@/assets/shopping.svg" class="img-fuid">
+        </div>
+    </div>
+  </template>
+  <template #rightCol>
+    <RegisterComponent />
+  </template>
+  </TheTwoColumnsLayout>
+</template>
+
+<script>
+import TheTwoColumnsLayout from "@/layouts/TheTwoColumnsLayout";
+import RegisterComponent from "@/components/auth/RegisterComponent.vue";
+export default {
+  name: "HomePage",
+  components: {
+    TheTwoColumnsLayout,
+    RegisterComponent,
+  },
+};
+</script>
+
+<style scoped></style>
