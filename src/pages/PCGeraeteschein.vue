3996ec5be63e0ebf2fee06df7fd1d475e9fc6d9e
DatKHWOLF <dkhwolf@gmail.com>
2024-11-25 12:04:24 +0100

    Fehler von fehlenden Components behoben

--- src/pages/PCGeraeteschein.vue
+++ src/pages/PCGeraeteschein.vue
@@ -2,21 +2,77 @@
   <div>
     <TheShopLayout>
       <template #default>
-        <Form @submitPC="submitPC">
-            <div class="form-row">
-                <div class="form-group col-md8 offset-2">
-                    <label><strong>Hersteller</strong></label>
-                    <Field as="input" type="text" class="form-control-lg" id="hersteller" name="hersteller" placeholder="Hersteller eintragen"> </Field>
-                </div>
+        <Form>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="hersteller"><strong>Hersteller</strong></label>
+              <Field as="input" name="hersteller" type="text" class="form-control" id="hersteller"></Field>
             </div>
-            <div class="form-row">
-                <div class="form-group col-md8 offset-2">
-                    <label><strong>Prozessor</strong></label>
-                    <Field as="input" class="form-control form-control-lg" type="text" placeholder="Prozessor" name="prozessor" id="prozessor"></Field>
-                </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="prozessor"><strong>Prozessor</strong></label>
+              <Field as="input" name="prozessor" type="text" class="form-control" id="prozessor"></Field>
             </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="betriebssystem"><strong>Betriebssystem</strong></label>
+              <Field as="input" name="betriebssystem" type="text" class="form-control" id="betriebssystem"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="ram"><strong>RAM</strong></label>
+              <Field as="input" name="ram" type="text" class="form-control" id="ram"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="hersteller"><strong>Hersteller</strong></label>
+              <Field as="input" name="hersteller" type="text" class="form-control" id="hersteller"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="festplatte"><strong>Festplatte</strong></label>
+              <Field as="input" name="festplatte" type="text" class="form-control" id="festplatte"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="benchmarkSinglecore"><strong>Benchmark Single Core</strong></label>
+              <Field as="input" name="benchmarkSinglecore" type="text" class="form-control" id="benchmarkSinglecore"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="benchmarkMulticore"><strong>benchmarkMulticore</strong></label>
+              <Field as="input" name="benchmarkMulticore" type="text" class="form-control" id="benchmarkMulticore"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="bemerkungen"><strong>Bemerkungen</strong></label>
+              <Field as="input" name="bemerkungen" type="text" class="form-control" id="bemerkungen"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="benutzername"><strong>Benutzername</strong></label>
+              <Field as="input" name="benutzername" type="text" class="form-control" id="benutzername"></Field>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-8 offset-2">
+              <label for="passwort"><strong>Passwort</strong></label>
+              <Field as="input" name="passwort" type="text" class="form-control" id="passwort"></Field>
+            </div>
+          </div>
+          <button class="btn bg-vue">
+              <span>Speichern</span>
+            </button>
         </Form>
-
       </template>
     </TheShopLayout>
   </div>
@@ -24,11 +80,20 @@
 
 <script>
 import TheShopLayout from "@/layouts/TheShopLayout.vue";
+import { Form, Field } from "vee-validate";
+//import * as yup from "yup";
 export default {
   name: "PCGeraeteschein",
   components: {
     TheShopLayout,
+    Form,
+    Field,
   },
+  methods:{
+      submitPCData(values){
+        console.log(values)
+      }
+    },
   data(){
     return {
         hersteller : "",
@@ -37,11 +102,13 @@ export default {
         ram:"",
         festplatte:"",
         //Endkontrolle + Sichtkontrolle durch Yup
-        benchmark:"",
+        benchmarkSinglecore:"",
+        benchmarkMulticore:"",
         bemerkungen:"",
         benutzername:"user",
         passwort:"user",
     }
+
 
   }
 };
